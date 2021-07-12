b945d753c128dd2e3a297171e7014c5e667a0be4
Lars Flieger <a.purenetwork@gmail.com>
2021-07-12 11:31:20 +0200

    first commit

---
+++ src/app.model.ts
@@ -0,0 +1,22 @@
+import { IsString } from "class-validator"
+
+export class App {
+
+    @IsString()
+    test: string
+
+    /**
+     * Test autocompleten with: 
+    @IsBase32()
+    @IsArray()
+    @IsBoolean()
+    @IsNotEmpty()
+    @IsAscii()
+    @IsAlphanumeric()
+    @IsNotEmptyObject()
+    @IsLatLong()
+    @IsEthereumAddress()
+    base: strin
+     */
+
+}
